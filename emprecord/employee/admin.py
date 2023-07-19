c0bacf17d02d90f2c9c6370e434873f1b7edfb5f
shashank782 <107979272+shashank782@users.noreply.github.com>
2023-07-19 06:07:44 +0530

    Delete emprecord directory

--- emprecord/employee/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-admin.site.register(EmployeeDetail)
-admin.site.register(EmployeeEducation)
-admin.site.register(EmployeeExperience)
